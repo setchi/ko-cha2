7845fce3605f2a4dfa40448f495ee1fa653024d6
setchi <ri_xer@yahoo.co.jp>
2014-10-20 01:45:45 +0900

    使っていないアセットを削除

--- assets/js/sdk/facebook.js
+++
@@ -1,7 +0,0 @@
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/ja_JP/all.js#xfbml=1&appId=223934391105650";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
