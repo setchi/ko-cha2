d4f3846dd135a2d390a2a71ddc33856661f3daf2
setchi <ri_xer@yahoo.co.jp>
2015-03-22 08:13:29 +0900

    Update copyright

--- fuel/app/views/common/footer.php
+++ fuel/app/views/common/footer.php
@@ -1,3 +1,3 @@
 <footer>
-  <p class="muted credit pull-right">&copy;&nbsp;2014&nbsp;<a href="https://twitter.com/intent/user?screen_name=setchi_" target="_blank">setchi_</a></p>
+  <p class="muted credit pull-right">&copy;&nbsp;2014&nbsp;<a href="https://twitter.com/intent/user?screen_name=setchi" target="_blank">setchi</a></p>
 </footer>
