73dd877c26d2985b9eef6ff571a826f075b79615
setchi <iphase56@gmail.com>
2017-07-29 21:26:12 +0900

    description追記

--- fuel/app/classes/controller/chat.php
+++ fuel/app/classes/controller/chat.php
@@ -1,5 +1,24 @@
 <?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.7
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2013 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
 
+/**
+ * The Chat Controller.
+ *
+ * A basic controller example.  Has examples of how to set the
+ * response body and status.
+ *
+ * @package  app
+ * @extends  Controller
+ */
 class Controller_Chat extends Controller_Rest
 {
 	/**
--- fuel/app/classes/controller/room.php
+++ fuel/app/classes/controller/room.php
@@ -1,5 +1,23 @@
 <?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.7
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2013 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
 
+/**
+ * The Room Controller.
+ *
+ * チャットルーム生成・ルームアクセスに関する処理
+ *
+ * @package  app
+ * @extends  Controller
+ */
 class Controller_Room extends Controller_Rest
 {
 	/**
